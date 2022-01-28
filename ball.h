2b8000d1ccff64841608858c2c77b22eee5037a4
katiebarkun3 <92594853+katiebarkun3@users.noreply.github.com>
2022-01-28 19:30:13 +0300

    Add files via upload

---
+++ ball.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+class Ball {
+private:
+	sf::CircleShape circle;
+	sf::FloatRect hitbox;
+	float radius = 20.f;
+	float speedx = 5.f;
+	float speedy = -5.f;
+	size_t left_score = 0;
+	size_t right_score = 0;
+public: 
+	Ball();
+	sf::CircleShape getShape();
+	sf::Vector2f getPosition();
+	void update();
+	sf::FloatRect getHitBox();
+	void reboundHorizontal();
+	void reboundVertical();
+	size_t getLeftScore();
+	size_t getRightScore();
+};
